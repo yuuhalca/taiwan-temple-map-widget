68a52d0672953f835e53a1bf7722ad1fa71db5a9
yuuhalca <h.c.nicol@gmail.com>
2025-09-20 15:49:52 +0900

    styles add

--- widget-taiwan-temple-map.php
+++ widget-taiwan-temple-map.php
@@ -67,9 +67,33 @@ class Taiwan_Temple_Map_Widget extends Widget_Base {
         <script>
         var marker = [], infoWindow = [];
         var mapDiv = document.getElementById("map-canvas");
+        var mapStyles = [
+            {"elementType":"labels","stylers":[{"visibility":"off"},{"color":"#f49f53"}]},
+            {"featureType":"landscape","stylers":[{"color":"#f9ddc5"},{"lightness":-7}]},
+            {"featureType":"road","stylers":[{"color":"#813033"},{"lightness":43}]},
+            {"featureType":"poi.business","stylers":[{"color":"#645c20"},{"lightness":38}]},
+            {"featureType":"water","stylers":[{"color":"#1994bf"},{"saturation":-69},{"gamma":0.99},{"lightness":43}]},
+            {"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"#f19f53"},{"weight":1.3},{"visibility":"on"},{"lightness":16}]},
+            {"featureType":"poi.business"},
+            {"featureType":"poi.park","stylers":[{"color":"#645c20"},{"lightness":39}]},
+            {"featureType":"poi.school","stylers":[{"color":"#a95521"},{"lightness":35}]},
+            {},
+            {"featureType":"poi.medical","elementType":"geometry.fill","stylers":[{"color":"#813033"},{"lightness":38},{"visibility":"off"}]},
+            {},{},{},{},{},{},{},{},{},{},{},
+            {"elementType":"labels"},
+            {"featureType":"poi.sports_complex","stylers":[{"color":"#9e5916"},{"lightness":32}]},
+            {},
+            {"featureType":"poi.government","stylers":[{"color":"#9e5916"},{"lightness":46}]},
+            {"featureType":"transit.station","stylers":[{"visibility":"off"}]},
+            {"featureType":"transit.line","stylers":[{"color":"#813033"},{"lightness":22}]},
+            {"featureType":"transit","stylers":[{"lightness":38}]},
+            {"featureType":"road.local","elementType":"geometry.stroke","stylers":[{"color":"#f19f53"},{"lightness":-10}]},
+            {},{},{},
+        ];
         var opts = {
             center: new google.maps.LatLng(23.823538, 121.030859),
-            zoom: 7
+            zoom: 7,
+            styles: mapStyles
         };
         var map = new google.maps.Map(mapDiv, opts);
         var markerData = <?php echo json_encode($markerData, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES); ?>;
